529a0e74eeb935ded9469bccfedeb9e8c3ccba74
Ruth <ruthkeane166@gmail.com>
2020-03-27 15:41:50 +0000

    add

---
+++ Code/uk.R
@@ -0,0 +1,5 @@
+dataUK<-read.csv("../Data/Covid/uk/covid-19-uk-data/data/covid-19-cases-uk.csv")
+#PROB MOST USEFUL
+dataUK<-read.csv("../Data/Covid/uk/covid-19-uk-data/data/covid-19-totals-uk.csv")
+dataUK<-read.csv("../Data/Covid/uk/covid-19-uk-data/data/covid-19-indicators-uk.csv")
+#ABOVE HAS DEATHS TESTS AND CASES FOR DIFFERENT UK COUNTRIES
